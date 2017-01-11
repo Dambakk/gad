bb05e375f30270dd769b094378339143c6f68271
Christopher Dambakk <chrisd14@uia.no>
2017-01-11 12:43:58 +0100

    Changed file structure

--- wrapper/gad-wrapper.py
+++ wrapper/gad-wrapper.py
@@ -4,25 +4,12 @@ import argparse
 import json
 import os.path
 from PIL import Image
+import utils
 
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("inputImage", help="Path to the image to generate GUI from")
-parser.add_argument("outputPath", help="Path to where the generated project will be")
-parser.add_argument("projectName", help="Name of the project")
-
-platform = parser.add_mutually_exclusive_group(required=True)
-platform.add_argument("--ios", help="Create a iOs project", action="store_true")
-platform.add_argument("--html", help="Create a html web page", action="store_true")
-platform.add_argument("--android", help="Create an android project", action="store_true")
-
-logging = parser.add_mutually_exclusive_group(required=False)
-logging.add_argument("-s", "--silent", help="Silent output level", action="store_true", default=False)
-logging.add_argument("-d", "--default", help="Default ouput level", action="store_true", default=False)
-logging.add_argument("-v", "--verbose", help="Verbose output level", action="store_true", default=False)
-
-parser.add_argument("--debug", help="Run in debug mode", default=False)
+parser = utils.argparserSetup(parser)
 
 args = parser.parse_args()
 
@@ -30,11 +17,9 @@ print("This is the path for the image: "+ args.inputImage)
 print("This is the path for the output project: "+ args.outputPath)
 print("This is the name of the project: "+ args.projectName)
 
-
 if(args.ios): print("iOs is chosen")
 elif (args.html): print("HTML is chosen")
 elif (args.android): print("Android is chosen")
-
 
 outputLevel = 2
 if(args.silent): outputLevel = 1
@@ -61,44 +46,15 @@ if not(os.path.exists(args.inputImage)):
 # TODO: Do other pre checkings here...
 
 
+
 #Check if there is the file is
 
-def checkWhatFileType():
-	if(args.ios):
-		return ".ipa";
-	elif(args.html):
-		return ".html"
-	elif(args.android):
-		return ".txt"
 
-def checkIfDirExists():
-	newFolder = args.outputPath + "/" + args.projectName
-	fileName = checkWhatFileType()
-	if not os.path.exists(args.outputPath):
-		print("there is no such directory!!")
-		os.makedirs(newFolder)
-		f = open(newFolder+"/testFil" + fileName, "w+").close
-		print(newFolder)
-	else:
-		print("this path is already taken, try another")
-	return;
-
-checkIfDirExists()
+utils.checkIfDirExists(args)
 
 
+print("DONE!")
 
-"""
-print("File exists")
-im = Image.open(args.inputImage)
-im.rotate(45).show();
-"""
-
-"""
-with open(args.inputImage, "r+") as f: # with keyword instead of try catch
-	for line in f:
-		print(line)
-	json.dump(x, f)
-"""
 
 
 # TODO: Call the image parser here....
---
+++ wrapper/utils.py
@@ -0,0 +1,47 @@
+
+import os.path
+import argparse
+
+def checkWhatFileType(args):
+	if(args.ios):
+		return ".ipa";
+	elif(args.html):
+		return ".html"
+	elif(args.android):
+		return ".txt"
+
+def checkIfDirExists(args):
+	newFolder = args.outputPath + "/" + args.projectName
+	fileName = checkWhatFileType(args)
+	if not os.path.exists(args.outputPath):
+		print("Creating a folder " + newFolder + " in " + args.outputPath)
+		os.makedirs(newFolder)
+		f = open(newFolder+"/testFil" + fileName, "w+").close
+		print(newFolder)
+	else:
+		#As user if he wants to overwrite what is already there?
+		answer = input("Are you sure you want to overwrite? (y/n) ")
+		print("Your answer: " + answer)
+		print("this path is already taken, try another")
+	return;
+
+
+def argparserSetup(parser):
+
+	parser.add_argument("inputImage", help="Path to the image to generate GUI from")
+	parser.add_argument("outputPath", help="Path to where the generated project will be")
+	parser.add_argument("projectName", help="Name of the project")
+
+	platform = parser.add_mutually_exclusive_group(required=True)
+	platform.add_argument("--ios", help="Create a iOs project", action="store_true")
+	platform.add_argument("--html", help="Create a html web page", action="store_true")
+	platform.add_argument("--android", help="Create an android project", action="store_true")
+
+	logging = parser.add_mutually_exclusive_group(required=False)
+	logging.add_argument("-s", "--silent", help="Silent output level", action="store_true", default=False)
+	logging.add_argument("-d", "--default", help="Default ouput level", action="store_true", default=False)
+	logging.add_argument("-v", "--verbose", help="Verbose output level", action="store_true", default=False)
+
+	parser.add_argument("--debug", help="Run in debug mode", default=False)
+
+	return parser
