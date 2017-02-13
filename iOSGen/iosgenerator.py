0684c31989d70ed05c317533348339e9a84db84f
Christopher Dambakk <chrisd14@uia.no>
2017-02-13 12:26:27 +0100

    Added colors and stuff

--- iOSGen/iosgenerator.py
+++ iOSGen/iosgenerator.py
@@ -46,20 +46,13 @@ def copyProject(outputPath, args):
         fromDirectory = "DemoApp"
         toDirectory = outputPath
         print("Output path: ", outputPath)
-        print()
         copy_tree(fromDirectory, toDirectory)
 
         configSetup()
 
-        print(listenM, " is listenM") # To the *.m file
-        print(listenMP, " is listenMP") # to the *.h file 
-        print(listenH, " is listenH") # also to the .m file
-        print("        ")
-
         newListeM = []
         newListeH = []
         newListeMP = []
-        #TODO : Generate extractor code from JSON
 
         with open(args.jsonPath) as data_file:
             data = OrderedDict()
@@ -70,14 +63,9 @@ def copyProject(outputPath, args):
         	    return
 
             for e in data.items():
-                print("element: ", e )
                 readElement(e[1][1], newListeM, newListeH, newListeMP)
         print()
-        print("DONE reading elements")
-        #print(newListeM, " is newListeM") # To the *.m file
-        #print(newListeH, " is newListeH") # to the *.h file 
-        #print(newListeMP, " is newListeMP") # also to the .m file
-        #print("        ")
+        print("Done reading elements")
 
         replaceText(outputPath, "m", newListeM, newListeMP)
         replaceText(outputPath, "h", newListeH, None)
@@ -100,23 +88,22 @@ def readConfigFile(config):
             listenMP[option] = eval(config.get("listenMP", option))
         else:
             listenMP["#" + option] = eval(config.get("listenMP", option))
-
-
     options3 = config.options("listenH")
     for option in options3:
         listenH["#" + option] = eval(config.get("listenH", option))
 
-
+def hex_to_rgb(value):
+    """Return (red, green, blue) for the color given as #rrggbb."""
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
 def saveIOSobjectM(color, elementId, posX, posY, width, height, liste, newListeM):
-    #print(color, elementId, posX, posY, width, height, "  hahahadsfasdfasdfs")
-
     if(color in liste):
         name = liste[color][0][0] + str(elementId)
         newItem = ""
         for i in range(1, len(liste[color])):
             item = liste[color][i]
-
             if ("NAME" in item[0]):
                 newItem = str(item[0]).replace("NAME", str(name))
             if ("posX" in item[0]):
@@ -128,9 +115,10 @@ def saveIOSobjectM(color, elementId, posX, posY, width, height, liste, newListeM
             if ("height" in item[0]):
                 newItem = str(newItem).replace("height", str(height))
             if ("COLOR" in item[0]):
-                newItem = str(newItem).replace("COLOR", "redColor") #Need some work
+                (r,g,b) = hex_to_rgb(color)
+                rgbColor = "colorWithRed:{0} green:{1} blue:{2} alpha:1".format(r,g,b)
+                newItem = str(newItem).replace("COLOR", rgbColor) 
             newListeM.append(newItem)
-
     else:
         print("Not a valid color")
 
@@ -147,21 +135,19 @@ def saveIOSobjectMP(newListeMP, parentColor, elementId, parent, listenMP, color)
         if(color in listenMP):
             name = listenM[color][0][0] + str(elementId)
             item = listenMP[parentColor][0]
-            print("ITEM: ", item[0])
             newItem = ""
             if("ID" in item[0]):
-                newItem = str(item).replace("ID", str(parent))
+                newItem = str(item[0]).replace("ID", str(parent))
             if("NAME" in item[0]):
                 newItem = str(newItem).replace("NAME", str(name))
             newListeMP.append(newItem)
     else:
         if(color in listenMP):
-            print(listenMP)
             item = listenMP['default'][0]
             name = listenM[color][0][0] + str(elementId)
             newItem = ""
             if("NAME" in item[0]):
-                newItem = str(item).replace("NAME", str(name))
+                newItem = str(item[0]).replace("NAME", str(name))
             newListeMP.append(newItem)
 
 
@@ -186,22 +172,12 @@ def readElement(element, newListeM, newListeH, newListeMP):
     if len(contentStructure) > 0:
         for i in range (0, len(contentStructure)):
             content = readElement(contentStructure[str(i)][1], newListeM, newListeH, newListeMP)
-            
-        #print("Found some content")
-        #print(contentStructure['0'][1])
-    		#Should might be another loop here to loop through the content instead of hard coding in '0'
-            # Must be!!!!! 
-            # of course it should ;)
-        #content = readElement(contentStructure['0'][1], newListeM, newListeH, newListeMP)
-
+      
 
 def replaceText(templatePath, fileType, elements, elements2):
     templatePath = templatePath+"/DemoApp"
-    print(templatePath)
     file1 = open(templatePath + "/ViewControllerBase." + fileType , "r")
     file2 = open(templatePath + "/NewFile.txt", "w")
-    #Hvorfor skrive til newFile og ikke rett til den nye filen?
-    #Da vil man vel slippe røkelet under og bare ta re.search() og kun replace teksten som skal endres?
 
     for line in file1:
         matchObj = re.match('\{\{\[\]\}\}', line)
@@ -215,7 +191,6 @@ def replaceText(templatePath, fileType, elements, elements2):
                 for j in elements2:
                     file2.write(j + "\n")
         else:
-            #print(line)
             file2.write(line)
     file1.close()
     file2.close()
@@ -235,15 +210,12 @@ def replaceText(templatePath, fileType, elements, elements2):
 if __name__== "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("jsonPath", help="Path to JSON structure")
-    #ap.add_argument("title", help="The title of the web page")
     ap.add_argument("outputPath", help="Output directory")
-    #ap.add_argument("jsonPath", help="Path to JSON structure")
-    #ap.add_argument("test", help="hihi")
     platform = ap.add_mutually_exclusive_group(required=True)
     platform.add_argument("--ios", help="Create a iOs project", action="store_true")
     platform.add_argument("--html", help="Create a html web page", action="store_true")
     platform.add_argument("--android", help="Create an android project", action="store_true")
     ap.add_argument("-v", "--verbose", help="Verbose output", action="store_true", default=False)
     args = ap.parse_args()
-    #Start the parser
+    
     copyProject(args.outputPath, args)
